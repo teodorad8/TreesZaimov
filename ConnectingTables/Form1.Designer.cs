e534dcffdfa81a7bd3a0c33f8b0374ef5d981403
teodorad8 <teodorad48@gmail.com>
2024-04-16 13:59:09 +0300

    gotova tablica rod

---
+++ ConnectingTables/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ConnectingTables
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtNameBG = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(87, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(248, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name_bg";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(47, 76);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtNameBG
+            // 
+            this.txtNameBG.Location = new System.Drawing.Point(252, 76);
+            this.txtNameBG.Name = "txtNameBG";
+            this.txtNameBG.Size = new System.Drawing.Size(100, 20);
+            this.txtNameBG.TabIndex = 3;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn1.Location = new System.Drawing.Point(132, 127);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(92, 55);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "Insert into ROD";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 221);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtNameBG);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Connecting Tables";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtNameBG;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ ConnectingTables/Form1.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectingTables
+{
+    public partial class Form1 : Form
+    {
+        string connstr = "server=10.6.0.127;port=3306;user=PC1;password=1111;database=trees_zaimov";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+           
+            
+            string insertSQL = "INSERT INTO trees_zaimov.rod" +
+                "(`name`,name_bg)" + "VALUES (@name,@name_bg)";
+            MySqlConnection connect = new MySqlConnection(connstr);
+            if (connect.State == 0) connect.Open();
+            MessageBox.Show("Connection is now opened!");
+            MySqlCommand query = new MySqlCommand(insertSQL, connect);
+            query.Parameters.AddWithValue("@name", txtName.Text);
+            query.Parameters.AddWithValue("@name_bg",txtNameBG.Text);
+
+            query.ExecuteNonQuery();
+            MessageBox.Show("Add ok ");
+            connect.Close();
+
+
+
+        }
+    }
+}
